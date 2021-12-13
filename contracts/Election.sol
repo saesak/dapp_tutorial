399c285c23bec9007e8de7e3d2a7e59748188b7b
saesak <matthewcho2000@gmail.com>
2021-12-13 22:58:56 +0900

    extra comment

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,10 @@ contract Election {
     }
 
     //think he wanted this in the tutorial that executes on deployment
+    //used a constructor here instead of using Election like he did because
+    //in newer versions of solidity you can't name the function after the contract
+    //and so to execute something on creation of the contract, you must use constructors!
+    //but I just changed the name of the Election function to Elect, just in case I needed it later. 
 
     constructor () {
         candidateName = "Candidate 1";
